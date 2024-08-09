7f6d26c636443e111c4319b1409df7d73a04e269
Vardaan-02 <vardaanpahwa02@gmail.com>
2024-08-09 19:52:23 +0530

    login sign-up UI Done

--- comX-frontend/src/App.tsx
+++ comX-frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
 
 function App() {
   return (
     <>
+    <Signup></Signup>
+    {/* <Login></Login> */}
     </>
   );
 }
