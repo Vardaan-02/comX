0640d268eac6072e20bc1b73cb59046eab806db5
Vardaan-02 <vardaanpahwa02@gmail.com>
2024-08-22 14:38:08 +0530

    navbar bug fixed

--- comX-frontend/src/pages/Community.tsx
+++ comX-frontend/src/pages/Community.tsx
@@ -1,7 +1,7 @@
 import { SidebarMain } from "@/components/example/sidebar-main";
 
 function Community(){
-    return <SidebarMain/>
+    return <><SidebarMain/><div className="relative pl-20"><h1 className="text-white">Hello</h1></div></>
 }
 
 export default Community;
