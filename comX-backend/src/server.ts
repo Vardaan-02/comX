80417b8a6fb8c60f7c636e4db1a7f7ae98e7f030
Pratham Jain <jainpratham2601@gmail.com>
2024-10-02 19:38:29 +0530

    deploy hoja

--- comX-backend/src/server.ts
+++ comX-backend/src/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req: Request, res: Response)=>{
-    res.send("server running");
+    res.send("server is running");
 })
 
 const auth = require("./routes/auth.route");
